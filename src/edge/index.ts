afa5961abc3654776ff66531543f2422d824ca1a
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2024-01-27 21:45:25 -0500

    Add testing edge function

--- src/edge/index.ts
+++ src/edge/index.ts
@@ -4,9 +4,9 @@ import {
   serveStatic,
   secureHeaders,
 } from "https://deno.land/x/hono@v3.12.8/middleware.ts";
-import home from "./routes/home.tsx";
+import home from "../routes/home.tsx";
 
-import { jsxMiddleware } from "./middleware/jsxRenderer.tsx";
+import { jsxMiddleware } from "../middleware/jsxRenderer.tsx";
 import { handle } from "https://deno.land/x/hono@v3.12.8/adapter/netlify/handler.ts";
 
 const app = new Hono();
