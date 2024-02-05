c08a9cc4a1b70280bc6210fb6c3cd7bcc61ce1bd
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2024-02-04 19:30:27 -0500

    Update home placeholer

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,14 +1,27 @@
 import { Hono } from "hono";
-import { contentMiddleware } from "../middleware/content.ts";
 
 const home = new Hono();
 
 home.get("/", (ctx) => {
   return ctx.render(
-    <div class="py-4">
-      <h1 class="text-primary text-4xl">Francisco Veracoechea</h1>
-      <p>Software Enginier</p>
-      <main>Content</main>
+    <div class="flex flex-col gap-6 pt-8">
+      <h1 class="text-dark text-4xl font-medium">Homepage</h1>
+      <a href="/blog" class="text-primary font-medium underline">
+        blog
+      </a>
+      <p>
+        Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit
+        enim labore culpa sint ad nisi Lorem pariatur mollit ex esse
+        exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit
+        nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor
+        minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure
+        elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor
+        Lorem duis laboris cupidatat officia voluptate. Culpa proident
+        adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod.
+        Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim.
+        Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa
+        et culpa duis.
+      </p>
     </div>,
   );
 });
