d08f1613fce22185dd68c82895d18b791df22ac7
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2024-07-02 13:50:30 -0400

    Set dark-theme as default

--- src/theme.ts
+++ src/theme.ts
@@ -1,15 +1,15 @@
 const THEME = {
-  DARK: 'cat-mocha',
-  LIGHT: 'cat-latte',
-  SYSTEM: 'system-theme',
+  DARK: "cat-mocha",
+  LIGHT: "cat-latte",
+  SYSTEM: "system-theme",
 } as const;
 
-const theme = localStorage.getItem('theme');
-const html = document.querySelector('html');
+const theme = localStorage.getItem("theme");
+const html = document.querySelector("html");
 
 if (theme === THEME.DARK || theme === THEME.LIGHT || theme === THEME.SYSTEM) {
   html?.classList.add(theme);
 } else {
-  localStorage.setItem('theme', THEME.SYSTEM);
-  html?.classList.add(THEME.SYSTEM);
+  localStorage.setItem("theme", THEME.DARK);
+  html?.classList.add(THEME.DARK);
 }
