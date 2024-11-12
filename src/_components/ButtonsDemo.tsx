973ea14fa16a9e2bc6cd58814f6ea1d52113ba1f
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2024-11-11 19:44:53 -0500

    Improve cva arcticle's demo

--- src/_components/ButtonsDemo.tsx
+++ src/_components/ButtonsDemo.tsx
@@ -9,12 +9,11 @@ type Props = {
 export default function (props: Props) {
   const v = props.variant;
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-2 py-2">
       <div
         className={clsx(
-          "playground not-prose flex gap-4 bg-cat-mantle",
+          "playground not-prose flex gap-4",
           "flex-wrap items-center md:justify-between",
-          "rounded border border-cat-surface0 bg-cat-base p-4",
         )}
       >
         <Button modifier={v} color="primary" size="small">
