713e46dbd20019e10e6d56644d04e009f80dd524
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2026-04-26 09:55:12 -0400

    fix classnames

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -27,22 +27,24 @@ export default function Home() {
     <div className="pb-12 pt-8">
       <section class="flex flex-col items-center gap-8 rounded pb-20 md:flex-row md:pt-10">
         <div className="flex flex-1 flex-col gap-4">
-          <h2 className="text-4xl !leading-[1.15] md:text-5xl">
+          <h2 className="text-4xl leading-[1.15]! md:text-5xl">
             Web engineering <i className="text-ctp-blue">experimentation</i>,{" "}
             <i className="text-ctp-mauve">learning</i>, and{" "}
             <i className="text-ctp-flamingo">ideas</i> worth sharing in the world wide web.
           </h2>
           <SocialLinks size="lg" />
         </div>
-        <div className="w-full flex-[2] rounded-full">
+        <div className="w-full flex-2 rounded-full">
           <UndrawProgramming />
         </div>
       </section>
 
-      <section className="flex flex-col gap-6 pb-20">
-        <p className="prose text-lg">
-          Hello there! I'm Francisco, a web engineer passionate about experimenting, learning, and
-          building. This is my corner of the web where I share things I find worth sharing.
+      <section className="flex flex-col gap-4 pb-20">
+        <p className="text-lg font-semibold">Hello there</p>
+        <p className=" text-lg">
+          I'm Francisco, a software engineer specialized in web technologies, who really enjoys
+          figuring out technical challenges, learning, research, and loves building{" "}
+          <b>performant</b> and <b>polished</b> user interfaces.
         </p>
         <ul className={cx("flex flex-wrap gap-2 text-sm md:gap-8 md:text-base")}>
           {interests.map((x) => (
